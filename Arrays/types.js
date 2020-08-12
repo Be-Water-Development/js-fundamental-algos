c430be334e25c56a4a39102cdcc89b3c1f156827
Xavyr Moss <xavyr.moss@cybergrx.com>
2020-08-12 16:02:18 -0700

    fixing up

--- Arrays/types.js
+++ Arrays/types.js
@@ -1,27 +1,30 @@
-// Take this array and convert it to an object where the key is the element's index and the value is an object whose key is the type of the stringified element and the value is the type of the element. 
+// Take this array and convert it to an object where the key is the element's index and the value is an object whose key is the type of the stringified element and the value is the type of the element.
 
 // Bonus...what would happen to the result if you have two elements of the same type?
 
 const arr = [
-  'a',
+  "a",
   504,
-  [1,2,3],
-  { 'key': 'value'},
+  [1, 2, 3],
+  { key: "value" },
   true,
   null,
   undefined,
-  () => void
-]
+  () => console.log("this is a simple function"),
+];
 
+const { x } = {};
+const x = () => {
+  return "lol";
+};
 
-
-// returns 
+// returns
 // {
 //   0: { a: string },
 //   1: { 504: number },
 //   2: { '[1,2,3]': object },
-//   3: { true: boolean }, 
+//   3: { true: boolean },
 //   4: { null: object },
-//   5: { "undefined": "undefined" },  
-//   6: { "() => void": "function" }, 
+//   5: { "undefined": "undefined" },
+//   6: { "() => void": "function" },
 // }
--- Arrays/vowels.js
+++ Arrays/vowels.js
@@ -1,10 +1,7 @@
 // Create an array that has every letter in the alphabet (hint do not hardcode-- attempt this programatically). From this array, return a new array that contains only vowels. For bonus points use the function `filter`
 
+const letters = []; //create
 
-const letters = //create
+const v = () => {};
 
-const v = () => {
-
-}
-
-v(letters) // ['A', 'E', 'I', 'O', 'U', 'Y']
+v(letters); // ['A', 'E', 'I', 'O', 'U', 'Y']
