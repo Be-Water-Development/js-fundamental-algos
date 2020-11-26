a1831fb8a8b9417e54e22d473f0c4ffd2f53b499
Peter Klingman <peter@peters-air.home>
2020-11-25 17:56:23 -0800

    First array problem

--- Arrays/adjective_collecting.js
+++ Arrays/adjective_collecting.js
@@ -1,4 +1,8 @@
-// An adverb is a word that ends in 'ly'. Iterate over the array and return an object where each key is the adverb found in the array and the value is the number of characters of the word. For bonus points do it with the function reduce
+// An adverb is a word that ends in 'ly'. 
+// Iterate over the array and return an object 
+// where each key is the adverb found in the array 
+// and the value is the number of characters of the word. 
+// For bonus points do it with the function reduce
 
 const arr = [
   "verb",
@@ -11,6 +15,23 @@ const arr = [
   "silently",
 ];
 
-const adverbCollections = () => {};
 
-adverbCollections(arr);
+const adverbCollections = (array) => {
+  // iterate over the array with reduce
+  // return an object where each key is the adverb, and the value is the 
+  // number of chars
+  return array.reduce(cb = (acc, val) => {
+    // acc starts as an empty object
+    // use indexing to check if the last two letters of the word = "ly"
+    // conditional statement
+    if (val.slice(-2) == 'ly') {
+      // if the last two letters are "ly", set the word as the key
+      // and the value as the length of the key
+      acc[val] = val.length
+    }
+    return acc
+  }, {})
+};
+
+// test the output
+console.log(adverbCollections(arr));
