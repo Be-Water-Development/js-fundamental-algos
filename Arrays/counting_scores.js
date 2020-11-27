238e4bfa59e1f0a4965bb52589f260a5bfb14c45
Peter Klingman <peter@peters-air.home>
2020-11-27 11:05:41 -0800

    counting_scores and grocery_list answers

--- Arrays/counting_scores.js
+++ Arrays/counting_scores.js
@@ -1,3 +1,11 @@
 //Count the number of elements in scores that are 100 or above.
 
 let scores = [96, 47, 113, 89, 100, 102];
+
+const countScores = (arr) => {
+    return arr.filter((el) => {
+        return el >= 100
+    }).length
+}
+
+console.log(countScores(scores))
--- Arrays/grocery_list.js
+++ Arrays/grocery_list.js
@@ -1,4 +1,5 @@
-// We have made a grocery list, and as we check off items on that list, we would like to remove them.
+// We have made a grocery list, and as we check off items on that list, 
+// we would like to remove them.
 
 // Write code that removes the items from 'groceryList' one by one,
 // until it is empty. If you log the elements you remove,
@@ -16,6 +17,23 @@ let groceryList = [
 
 // Your code.
 
+// Iterate through the array
+// we need to reverse the array and iterate backwards, using arr.pop()
+
+const checkOffItems = (arr) => {
+  // reverse the array
+  arr.reverse()
+  // write a backwards for loop to iterate backwards through the array
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // log the elements as they're removed
+    console.log(arr[i])
+    // pop off the last element in the array
+    arr.pop()
+  }
+  // return the empty array
+  return arr
+}
+
 // logs:
 // paprika
 // tofu
@@ -25,4 +43,24 @@ let groceryList = [
 // broccoli
 // hummus
 
-groceryList; // []
+
+// check answers
+
+console.log("func output:", checkOffItems(groceryList)) // []
+console.log("list output:", groceryList); // []
+
+
+// Things I tried that did not work
+
+// why didn't this work? mutating the array affected the index
+
+ // also use the index and object params in forEach
+ // arr.forEach((el, index, arr) => {
+  // log the element to the console
+  // console.log(el)
+  // arr.splice(index, 1)
+  // console.log(arr)
+// })
+
+
+
