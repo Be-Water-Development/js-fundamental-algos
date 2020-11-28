cbe07173734d5d4af8a32762da45088b3a861deb
Peter Klingman <peter@peters-air.home>
2020-11-28 12:01:45 -0800

    four more array problems

--- Arrays/inventory_item_availability.js
+++ Arrays/inventory_item_availability.js
@@ -1,6 +1,13 @@
-// Building on the previous exercise, write a function that returns true or false based on whether or not an inventory item is available. As before, the function takes two arguments: an inventory item and a list of transactions. The function should return true only if the sum of the quantity values of the item's transactions is greater than zero. Notice that there is a movement property in each transaction object. A movement value of 'out' will decrease the item's quantity.
+// Building on the previous exercise, write a function that returns true or 
+// false based on whether or not an inventory item is available. As before, the 
+// function takes two arguments: an inventory item and a list of transactions. 
+// The function should return true only if the sum of the quantity values of the 
+// item's transactions is greater than zero. Notice that there is a movement 
+// property in each transaction object. A movement value of 'out' will decrease 
+// the item's quantity.
 
-// You may (and should) use the transactionsFor function from the previous exercise.
+// You may (and should) use the transactionsFor function from 
+// the previous exercise.
 
 const transactions = [
   { id: 101, movement: "in", quantity: 5 },
@@ -19,11 +26,31 @@ const transactionsFor = (inventoryItem, transactions) => {
   return transactions.filter((item) => item.id === inventoryItem);
 };
 
-const isItemAvailable = (item, transactions) => {
-  return;
-
-  transactions[item];
+const isItemAvailable = (inventoryItem, transactions) => {
+  // check if the item exists using transactionsFor function above
+  // movement in + the quantity
+  // return true if quantity is greater than 0
+  // initialize output and sum
+  let output = false
+  let sum = 0
+  // filter the item to id keys that match the input string, then 
+  // iterate through the resulting array of objects
+  transactions.filter((item) => item.id === inventoryItem).forEach(
+    (el) => {
+      if (el.movement === 'in') {
+        sum += el.quantity
+      } else {
+        sum -= el.quantity
+      }
+    })
+  // check the sum after the above operations, set to true if sum is greater
+  // than 0  
+  if (sum > 0) {
+    output = true
+  }
+  
+  return output
 };
 
-isItemAvailable(101, transactions); // false
-isItemAvailable(105, transactions); // true
+console.log(isItemAvailable(101, transactions)); // false
+console.log(isItemAvailable(105, transactions)); // true
--- Arrays/most_occuring_array_element.js
+++ Arrays/most_occuring_array_element.js
@@ -1,7 +1,42 @@
-// Build a function that takes in an array as a parameter. Return an object where the key is the element and the value is the number of occurrences for the the element that occurs the most often in the array. If there are ties return an object with all the highest occuring elements. If the paramenter passed in is not an array return null
+// Build a function that takes in an array as a parameter. 
+// Return an object where the key is the element and the value 
+// is the number of occurrences for the the element that occurs 
+// the most often in the array. If there are ties return an object 
+// with all the highest occuring elements. If the paramenter passed 
+// in is not an array return null
 
-const arr = [1, 4, 5, 3, 6, 7, 7, 4, 7, 7];
+const arr = [1, 4, 5, 3, 6, 7, 7, 4, 7, 7, 9, 9, 9 ,9];
+const obj = {"feet": "smelly"};
 
-const mode = () => {};
+// take an array as a parameter
+const mode = (arr) => {
+    // obj will store all the array elements and their frequencies
+    // handle scenarios where the input is not an array
+    if (!Array.isArray(arr)) {
+        return null
+    } else {
+        // create a dict of values and occurrences using reduce
+        let output = {};
+        let obj = arr.reduce((acc, val) => {
+            if (!acc[val]) {
+                acc[val] = 1
+            } else {
+                acc[val]++
+            }
+            return acc
+        }, {})
+        // find the greatest value in the object, handle multiple modes
+        // find the max value in the object 
+        const max = Math.max(...(Object.values(obj)))
+        // return key:value pairs to the output object if the value = max
+        for (const property in obj) {
+            if (obj[property] === max) {
+                output[property] = max
+            }
+        }
+        return output
+    }
+};
 
-mode(arr); // returns { 7: 4 }
+console.log(mode(arr)); // returns { '7': 4, '9', 4 }
+console.log(mode(obj)); // returns null
