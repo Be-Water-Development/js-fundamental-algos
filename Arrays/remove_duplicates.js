b8c187b259c59fd4c1c6fd8779652789aff42b68
Peter Klingman <peter@peters-air.home>
2020-11-29 08:19:56 -0800

    types and remove duplicates from the array prob set

--- Arrays/remove_duplicates.js
+++ Arrays/remove_duplicates.js
@@ -1,3 +1,22 @@
 // How would you remove duplicate members from an array?
 
-removeDuplicate([1, 3, 3, 3, 1, 5, 6, 7, 8, 1]); // return [1, 3, 5, 6, 7, 8]
+// Use reduce, push elements into an array if the key doesn't exist
+
+// Declare the function
+const removeDuplicate = (arr) => {
+    // empty array to push values
+    const output = []
+    // reduce with an empty obj as the acc
+    arr.reduce((acc, val) => {
+        if (!acc[val]) {
+            acc[val] = val;
+            output.push(val)
+        }
+        return acc
+    }, {})
+    return output
+}
+
+console.log(removeDuplicate([1, 3, 3, 3, 1, 5, 6, 7, 8, 1])); // return [1, 3, 5, 6, 7, 8]
+
+
