81154dcee97e895397938ddc49beb173180cf863
Peter Klingman <peter@peters-air.home>
2020-12-01 08:39:16 -0800

    after.js - complete and cached_math.js with questions

--- Closures/after.js
+++ Closures/after.js
@@ -4,12 +4,28 @@
 
 // ADD CODE HERE
 
+const after = (num, cb) => {
+  // declare variables needed in the closure
+  let counter = 0;
+  // declare inner function with available params
+  const innerFunc = (...params) => {
+    // increment the counter
+    counter ++
+    // once the counter is three or greater execute the cb
+    if (counter >= num) {
+      return cb(...params)
+    }
+
+  }
+  return innerFunc
+}
+
 const called = function (string) {
   return "hello " + string;
 };
 const afterCalled = after(3, called);
 
 // UNCOMMENT THESE LINES TO TEST YOUR WORK
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> 'hello world' is printed
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> 'hello world' is printed
