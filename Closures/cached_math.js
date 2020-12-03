86a6866cee26ca74de33d666fc189b9ced959b1f
Peter Klingman <peter@Peters-Air.lan>
2020-12-02 20:50:01 -0800

    cached_math.js

--- Closures/cached_math.js
+++ Closures/cached_math.js
@@ -1,45 +1,54 @@
-// Create a function that takes a parameter and a function and returns the time it took
-// for the callback function to fire. If the callback and parameter are exactly the same
-// returned the cached time value; don't run the function again.
+// Create a function called `timeIt` that takes a callback. 
+// `timeIt` should return a function
+// that caches how long it took for the callback to run IF 
+// its the first time it has been called.
+// If the same callback AND the same millisecond value is 
+// passed to `cachedTimeIt` then the callback should
+// NOT be executed again, but instead return the cached timed value.
 
-// The function
 
-const cachedTimeIt = (param, cb) => {
-  // closure
-  let cachedValue;
+
+// Code here
+const timeIt = (cb) => {
+  // store the cached time value in the closure
+  let cachedNum;
   let cachedCb;
-  const countSeconds = (...params) => {
-    // check if the passed in cb and param are equal to the cached ones
-    if (cachedValue === param && cachedCb === cb) {
-      // console.log(cachedValue)
-      return cachedValue
-    } else {
-      cachedValue = param;
+  // return a function
+  const innerFunc = (num) => {
+    if (!cachedNum) {
+      cachedNum = num;
       cachedCb = cb;
-      // console.log(cachedValue)
-      // return the time in seconds it took the cb to fire
-      setTimeout(cb, param*1000, ...params)
+      // use setTimeout to take num ms to run the cb
+      setTimeout(cb, num, num)
+      return num
+    } else if (cachedNum === num && cachedCb === cb) {
+      return cachedNum;
+    } else {
+      // invoke the callback and setTimeout to num ms
+      // return num
+      setTimeout(cb, num, num)
+      return num
     }
-  }
-  return countSeconds
-}
-
-
+  } 
+  return innerFunc
+};
 // The callback
 const countDown = (number) => {
   while (number > 0) {
+    // check if the callback is being invoked
+    console.log(number)
     number--;
-    console.log(number);
   }
 };
 
-// I added the cb param to cachedTimeIt here, "...takes a param and a function..."
-const output1 = cachedTimeIt(100, countDown); // should return timed value
-// Don't we need to invoke output1? Invoking with 100 achieves the console log 
-// from the inner function
-console.log(output1());
-const output2 = cachedTimeIt(100, countDown); // should return cached timed value without running the function
-// if using closure, this just returns the function definition
+const cachedTimeIt = timeIt(countDown);
+
+const output1 = cachedTimeIt(1000); // should return timed value
+console.log(output1);
+const output2 = cachedTimeIt(1000); // should return cached timed value without running the function
 console.log(output2);
-const output3 = cachedTimeIt(100, countDown); // should return same at output2
+const output3 = cachedTimeIt(500); // should invoke the cb and return timed value
 console.log(output3);
+const output4 = cachedTimeIt(1000); // should return the same as output 2
+console.log(output4);
+
