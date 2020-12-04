1173949828c7131b991d6f757b013f1633946353
“Noley <hollandnoley@gmail.com>
2020-12-04 14:37:28 -0800

    stringsSolutions

--- Strings/deleteVowels.js
+++ Strings/deleteVowels.js
@@ -1,5 +1,18 @@
-// Write a function that takes an array of strings, and returns an array of the same strings values without the vowels (a, e, i, o, u).
+// Write a function that takes an array of strings, and returns an array of 
+//the same strings values without the vowels (a, e, i, o, u).
 
-removeVowels(["abcdefghijklmnopqrstuvwxyz"]); // ["bcdfghjklmnpqrstvwxyz"]
-removeVowels(["green", "YELLOW", "black", "white"]); // ["grn", "YLLW", "blck", "wht"]
-removeVowels(["ABC", "AEIOU", "XYZ"]); // ["BC", "", "XYZ"]
+
+//delcare a function removeVowels, array as input 
+function removeVowels (array) {
+    
+    //using map, flip though the array, replace the vowels with an empty string, /g is a global replacement which targets all instances.
+    return array.map(item => item.replace(/[aeiouAEIOU]/g, ""));
+    
+}
+
+
+//uncomment for tests
+//console.log(removeVowels(["abcdefghijklmnopqrstuvwxyz"])); // ["bcdfghjklmnpqrstvwxyz"]
+//console.log(removeVowels(["green", "YELLOW", "black", "white"])); // ["grn", "YLLW", "blck", "wht"]
+//console.log(removeVowels(["ABC", "AEIOU", "XYZ"])); // ["BC", "", "XYZ"]
+
