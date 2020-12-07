b0df26c35f67798a79fb7d88c9df5c07aa1f3324
“Noley <hollandnoley@gmail.com>
2020-12-07 10:13:49 -0800

    closures3of4

--- Closures/after.js
+++ Closures/after.js
@@ -4,12 +4,28 @@
 
 // ADD CODE HERE
 
+
+// declare function after, num and callback as parameters 
+function after (num, callback) {
+  //set a counter equal to 0
+  let counter = 0; 
+  //set an interior function, string as parameter 
+  return function (...string) {
+    //increment counter by one on each pass
+    counter ++
+    //if the counter is equal to the num passed in, return the callback of the string 
+    if (counter >= num) {
+      return callback(...string)
+    }
+  }
+}
+
 const called = function (string) {
   return "hello " + string;
 };
 const afterCalled = after(3, called);
 
 // UNCOMMENT THESE LINES TO TEST YOUR WORK
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> 'hello world' is printed
+ console.log(afterCalled('world')); // -> undefined is printed
+ console.log(afterCalled('world')); // -> undefined is printed
+ console.log(afterCalled('world')); // -> 'hello world' is printed
--- Closures/once.js
+++ Closures/once.js
@@ -1,6 +1,31 @@
 // ADD CODE HERE
 
-const once = () => {};
+//declare function once, takes a callback as the parameter 
+function once(callback) {
+  //set a counter equal to false
+  let counter = false
+  //declare an empty variable to store the original value in 
+	let result;;
+  
+  //declare an interior function, num as parameter 
+	return function (num) {
+    //if counter is falsey, set it to true 
+    if (!counter) {
+      counter = true;
+    //store the result of the callback function in result
+      result = callback(num);
+    //return the result of callback function 
+      return callback(num);
+      } 
+    // if counter = true, return original result from first pass 
+    return result;
+      
+    }
+  
+}
+
+
+//const once = () => {};
 
 const addByTwoOnce = once(function (num) {
   return num + 2;
