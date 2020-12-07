11d55faffade6e9db6b4a7c6ee23f4ade8dad507
“Noley <hollandnoley@gmail.com>
2020-12-07 14:03:50 -0800

    HOF

--- Higher_Order_Functions/callback_filter.js
+++ Higher_Order_Functions/callback_filter.js
@@ -6,6 +6,38 @@
 
 // Create two callbacks that return truthy or falsey values
 
-const neitherFilter = (arr, first, second) => {};
 
-console.log(neitherFilter(["a", "b", "c", "D", "O", "o"])); //pass in  your created callbacks
+//declare function cb1, truthy values are in all uppercase
+function cb1(item) {
+    if (item === item.toUpperCase()) {
+        return item
+    }
+}
+
+//declare function cb2, truthy values include "o"
+function cb2(item) {
+    if (item.includes("o")) {
+        return item
+    }
+
+}
+
+
+//declare the function neitherFilter, takes array and two callbacks as parameters
+const neitherFilter = (arr, first, second) => {
+    //declare an empty array to push results into 
+    let outArray = []
+    //using forEach, flip over each item, if the element is a falsey return value of both the callbacks, push into outArray. 
+    arr.forEach((ele) => {
+        if (ele !== first(ele) && ele !== second(ele)) {
+            outArray.push(ele)
+        }
+    });
+    //return the outArray 
+    return outArray;
+};
+
+
+//uncomment for tests
+//console.log(neitherFilter(["a", "b", "c", "D", "O", "o"], cb1, cb2)); // logs: [ 'a', 'b', 'c' ]
+//console.log(neitherFilter(["frogs", "GREEN", "green", "TREE", "tree", "FROGS"], cb1, cb2)) // logs [ 'green', 'tree' ]
--- Higher_Order_Functions/times_five.js
+++ Higher_Order_Functions/times_five.js
@@ -1,5 +1,4 @@
-// Create a function subtractTwo that accepts a number and returns that number minus 2.
-
+// Create a function subtractTwo(timesFive????) that accepts a number and returns that number minus 2.(number times five)
 // Then create a function map that takes two inputs -
 
 // an array of numbers (a list of numbers)
@@ -9,7 +8,26 @@
 
 // ADD CODE HERE
 
+//declare a function timesfive, takes a number as input. 
+function timesFive(num) {
+    return num * 5;
+}
+
+//delcare a function map, takes an array and a callback
+function map(array, callback) {
+    //delcare an empty array to store results in 
+    let outArray = [];
+    //using a for loop, loop through the array, pushing the callback of each element into the outArray 
+    for (let i=0; i<array.length; i++) {
+        outArray.push(callback(array[i]))
+    }
+    //return the outArray
+    return outArray;
+}
+
+
+
 // Uncomment these to check your work!
-// console.log(typeof timesFive); // should log: 'function'
-// console.log(typeof map); // should log: 'function'
-// console.log(map([3,4,5], timesFive)); // should log: [ 15, 20, 25 ]
+ //console.log(typeof timesFive); // should log: 'function'
+ //console.log(typeof map); // should log: 'function'
+ //console.log(map([3,4,5], timesFive)); // should log: [ 15, 20, 25 ]
