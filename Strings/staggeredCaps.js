74a8a4629250fe160d68f65b6927d9e3ce128e51
Peter Klingman <peter@peters-air.home>
2020-12-08 19:21:06 -0800

    final three string problems

--- Strings/staggeredCaps.js
+++ Strings/staggeredCaps.js
@@ -1,7 +1,24 @@
-// Write a function that takes a string as an argument, and returns that string with a staggered capitalization scheme.
-// Every other character, starting from the first, should be capitalized and should be followed by a lowercase or non-alphabetic character.
-// Non-alphabetic characters should not be changed, but should be counted as characters for determining when to switch between upper and lower case.
+// Write a function that takes a string as an argument, and returns 
+// that string with a staggered capitalization scheme.
+// Every other character, starting from the first, should be 
+// capitalized and should be followed by a lowercase or non-alphabetic character.
+// Non-alphabetic characters should not be changed, 
+// but should be counted as characters for determining when 
+// to switch between upper and lower case.
 
-staggeredCase("I Love Coding Javascrtipt!"); // "I LoVe cOdInG JaVaScRiPt!"
-staggeredCase("ALL_CAPS"); // "AlL_CaPs"
-staggeredCase("ignore 77 the 4444 numbers"); // "IgNoRe 77 ThE 4444 nUmBeRs"
+const staggeredCase = (str) => {
+    // we can use even indices to capitalize and odd indices to lowercase
+    return str.split('').map((el, ind, arr) => {
+        if (el.match(/[a-zA-z]/) && ind % 2 === 0) {
+            return el.toUpperCase()
+        } else if (el.match(/[a-zA-z]/) && ind % 2 === 1) {
+            return el.toLowerCase()
+        } else {
+            return el;
+        }
+    }).join('')
+}
+
+console.log(staggeredCase("I Love Coding Javascript!")); // "I LoVe cOdInG JaVaScRiPt!"
+console.log(staggeredCase("ALL_CAPS")); // "AlL_CaPs"
+console.log(staggeredCase("ignore 77 the 4444 numbers")); // "IgNoRe 77 ThE 4444 nUmBeRs"
--- Strings/upperCase_check.js
+++ Strings/upperCase_check.js
@@ -1,8 +1,20 @@
-//Write a function that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase; false otherwise. Ignore characters that are not alphabetic.
+//Write a function that takes a string argument, and returns true 
+// if all of the alphabetic characters inside the string are uppercase; 
+// false otherwise. Ignore characters that are not alphabetic.
 
-isUppercase("t"); // false
-isUppercase("T"); // true
-isUppercase("Four Score"); // false
-isUppercase("FOUR SCORE"); // true
-isUppercase("4SCORE!"); // true
-isUppercase(""); // true
+const isUppercase = (str) => {
+    output = true
+    str.split('').forEach((el) => {
+        if (new RegExp(/[a-z]/).test(el)) {
+            output = false
+        }
+    })
+    return output
+}
+
+console.log(isUppercase("t")); // false
+console.log(isUppercase("T")); // true
+console.log(isUppercase("Four Score")); // false
+console.log(isUppercase("FOUR SCORE")); // true
+console.log(isUppercase("4SCORE!")); // true
+console.log(isUppercase("")); // true
