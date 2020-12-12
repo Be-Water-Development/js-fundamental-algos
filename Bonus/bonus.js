e0f22d76f6ba313dfc2282b8cf0b6e4dc99c6cda
“Noley <hollandnoley@gmail.com>
2020-12-11 17:20:38 -0800

    bonussolutions

--- Bonus/bonus.js
+++ Bonus/bonus.js
@@ -1,10 +1,19 @@
+
+
 // Creates a clone of an object.
-// const users = [{ 'user': 'barney' },{ 'user': 'fred' }];
-// const shallowClone = clone(users);
-// shallowClone[0] === users[0] → true
+const users = [{ 'user': 'barney' },{ 'user': 'fred' }];
+const shallowClone = clone(users);
+//shallowClone[0] === users[0] → true
+
 function clone(value) {
-  // CODE HERE
+  const clone = [];
+  Object.assign(clone, value);
+  return clone
 }
+
+//console.log(shallowClone); // [ { user: 'barney' }, { user: 'fred' } ]
+//console.log(shallowClone[0] === users[0]); // true 
+
 
 // Return the size of collection. If the argument passed is an array, then return
 // the length of the array. If the argument passed is an object, then return the
@@ -12,16 +21,33 @@ function clone(value) {
 // size([1,2,3]); → 3
 // size({a: 1, b: 2}); → 2
 function size(collection) {
-  // CODE HERE
+  if (Array.isArray(collection)) {
+    return collection.length;
+  } else if (typeof collection == "object") {
+    return Object.keys(collection).length;
+  }
 }
+
+//console.log(size([1,2,3])); // 3
+//console.log(size({a: 1, b: 2})); // 2
+
 
 // Returns the first element of an array without modifying the original array.
 // Returns undefined if array is empty
 // first([1,2,3]); → 1
 // first([]); → undefined
+
 function first(array) {
-  // CODE HERE
+  return array[0];
 }
+
+//Is this right?? Feel like that is too simple... 
+let arrFirst = [1, 2, 3]
+//console.log(first(arrFirst)); // 1
+//console.log(first([])); // undefined
+//console.log(arrFirst); // [ 1, 2, 3 ]
+
+
 
 // Creates a slice of array with n elements dropped from the beginning.
 // n defaults to 1
@@ -29,9 +55,17 @@ function first(array) {
 // drop([1, 2, 3], 2); → [3]
 // drop([1, 2, 3], 5); → []
 // drop([1, 2, 3], 0); → [1, 2, 3]
+
 function drop(array, n) {
-  // CODE HERE
+  if(!n && n !== 0) {n = 1};
+  return array.slice(n)
 }
+
+//console.log(drop([1, 2, 3])); //[ 2, 3 ]
+//console.log(drop([1, 2, 3], 2)); // [ 3 ]
+//console.log(drop([1, 2, 3], 5)); // []
+//console.log(drop([1, 2, 3], 0)); // [1, 2, 3]
+
 
 //Creates a slice of array with n elements taken from the beginning.
 //n defaults to 1
@@ -39,49 +73,174 @@ function drop(array, n) {
 // take([1, 2, 3], 2); → [1, 2]
 // take([1, 2, 3], 5); → [1, 2, 3]
 // take([1, 2, 3], 0); → []
+
 function take(array, n) {
-  // CODE HERE
+  if(!n && n !== 0) {n = 1};
+  return array.slice(0, n)
 }
+
+//console.log(take([1, 2, 3])); //[ 1 ]
+//console.log(take([1, 2, 3], 2)); //[ 1, 2 ]
+//console.log(take([1, 2, 3], 5)); //[ 1, 2, 3 ]
+//console.log(take([1, 2, 3], 0)); //[]
+
+
 
 // Gets the value of key from all elements in collection.
 // pluck([{user: 'Bob', age: 20},{user: 'Sam', age: 25}], 'user'); → ['Bob','Sam']
+
 function pluck(array, key) {
-  // CODE HERE
+  
+   return array.map((ele) => {
+    return ele[key]
+  });
+  
 }
+
+//I originally used forEach, then saw your video and changed to map, forgive me. 
+//console.log(pluck([{user: 'Bob', age: 20},{user: 'Sam', age: 25}], 'user')) // ['Bob','Sam']
+
 
 // Assigns own enumerable properties of source object(s) to the destination
 // object. Subsequent sources overwrite property assignments of previous sources.
 // extend({ 'user': 'barney' }, { 'age': 40 }, { 'user': 'fred' });
 // should return ->  { 'user': 'fred', 'age': 40 }
 // BONUS: solve with reduce
-function extend(destination) {
-  // CODE HERE
+function extend(...destination) {
+  return destination.reduce((acc, item) => {
+    return Object.assign(acc, item)
+  }, {});
+ 
 }
+
+//Was this supposed to be an array of objects? I used rest operator to accept multiple arguments in destinations. Hope that is okay. 
+//console.log(extend({ 'user': 'barney' }, { 'age': 40 }, { 'user': 'fred' })); // { 'user': 'fred', 'age': 40 }
+
+
 
 // Using a for loop, call the functions in the queue in order with the input
 // number, where the results of each function become the next function’s input.
 // Additionally, the queue should be empty after the function is called.
-/* const puzzlers = [
+ const puzzlers = [
   function(a) { return 8 * a - 10; },
   function(a) { return (a - 3) * (a - 3) * (a - 3); },
   function(a) { return a * a + 4;},
   function(a) { return a % 5;}
 ];
-const start = 2;
-applyAndEmpty(2, puzzlers); → 3
-*/
+//const start = 2;
+//applyAndEmpty(2, puzzlers); → 3
+
 function applyAndEmpty(input, queue) {
-  // CODE HERE
-}
+  
+//Exit case, when queue is empty, return the input 
+  if (queue.length === 0) {return input};
+
+  //for loop
+  for (let i = 0; i<queue.length; i++) {
+      //assign new input to result of invoking queue at index 0 with input
+      let newInput = queue[0](input);
+      //remove queue at index 0 
+      queue.splice(0, 1);
+      //call the function again with newinput and queue 
+      return applyAndEmpty(newInput, queue);
+    } 
+  }
+
+
+//console.log(applyAndEmpty(2, puzzlers)); // 3
+
+
+
 
 // Returns a function that when called, will check if it has already computed
 // the result for the given argument and return that value instead if possible.
 function memoize(func) {
-  // CODE HERE
+  let ran = false;
+  let result;
+  let firstRunNum; 
+
+  return function (num) {
+    if (ran === true && firstRunNum === num) {return `This function has already been run, the result is ${result}`};
+    if (ran === true && firstRunNum !== num) {ran = false};
+    if (!ran) {
+      ran = true;
+      result = func(num);
+      firstRunNum = num; 
+      return result;
+  } else {return result}
+  }
 }
+
+const funcMemo = memoize (function (num) {
+  return num * 2;
+});
+
+//console.log(funcMemo(2)); // 4
+//console.log(funcMemo(2)); // This function has already been run, the result is 4
+//console.log(funcMemo(5)); // 10
+//console.log(funcMemo(5)); // This function has already been run, the result is 10
+
+
+
+
+
+
+//I apologize in advance for the struggle below... 
+
+
+
 
 // Invokes func after wait milliseconds. Any additional arguments are provided
 // to func when it is invoked.
 function delay(func, wait) {
-  // CODE HERE
+
+  return function () {
+     setTimeout(func, wait);
+  }
 }
+
+const print = delay(function () {
+  console.log("is this right?");
+}, 2000);
+  
+
+//console.log(print()); // undefined, waits two seconds, "is this right?"
+
+// I had some trouble here, I know there is some closure holes in my brain. I couldn't get print to accept any arguments without getting the error
+//"TypeError [ERR_INVALID_CALLBACK]: Callback must be a function. Received undefined" 
+//I know we will review cached_math at some point, if we could touch base on this one too that would be wonderful. I feel like my biggest knowledge gap
+//is closurers. Here is what I struggled with: 
+
+
+function delayNaN(func, wait) {
+
+  return function () {
+     setTimeout(func, wait);
+  }
+}
+
+const printNaN = delayNaN(function (num) {
+  console.log(num * 2);
+}, 2000);
+  
+
+//console.log(printNaN(2)); // undefined, waits two seconds, NaN
+
+//so then I tried to pass the interior function a parameter num and invoke func with num, got the error:
+// "TypeError [ERR_INVALID_CALLBACK]: Callback must be a function. Received undefined" 
+
+function delayErr(func, wait) {
+
+  return function (num) {
+     setTimeout(func(num), wait);
+  }
+}
+
+const printError = delayErr(function (num) {
+  console.log(num * 2);
+}, 2000);
+
+//console.log(printError(2)) // "TypeError [ERR_INVALID_CALLBACK]: Callback must be a function. Received undefined" 
+//I did research the error, and I think I understand that func being passed a parameter is converting it into it's own function, not a callback. 
+//So I am a little lost on the connection between func, and it's access to the parameter num. I realize "Any additional arguments are provided
+// to func when it is invoked" is not connecting in my brain.  
