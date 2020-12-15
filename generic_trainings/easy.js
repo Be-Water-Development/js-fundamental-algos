4a9213ade24769dc4ea62fa21d8d76d60ff8154c
Peter Klingman <peter@peters-air.home>
2020-12-15 08:40:40 -0800

    rest of bonus, easy generic trainings, all of med. besides createArray

--- generic_trainings/easy.js
+++ generic_trainings/easy.js
@@ -1,106 +1,114 @@
+console.log("-----Simple Math Test Cases-----")
 // adds 2 to inputted number
-// ex: addTwo(5); -> 7
-// ex: addTwo(6); -> 8
+console.log("addTwo:", addTwo(5)); // 7
+console.log("addTwo:", addTwo(6)); // 8
 function addTwo(number) {
-  //CODE HERE
+  return number + 2;
 }
 
 // adds the two inputted numbers together
-// ex: add(5, 10); -> 15
+console.log("Add:", add(5, 10)); // 15
 function add(num1, num2) {
-  //CODE HERE
+  return num1 + num2;
 }
 
 // adds all three inputted numbers together
-// ex: addd(1,2,3); -> 6
-// ex: addd(2,4,2); -> 8
+console.log("Addd:", addd(1,2,3)); // 6
+console.log("Addd:", addd(2,4,2)); // 8
 function addd(value1, value2, value3) {
-  //CODE HERE
+  return value1 + value2 + value3;
 }
 
 // subtracts the second inputted number from the first inputted number
-// ex: subtract(5, 10); -> -5
+console.log("Subtract:", subtract(5, 10)); // -5
 function subtract(n1, n2) {
-  //CODE HERE
+  return n1 - n2;
 }
 
 // multiplies the inputted number by 2
-// ex: multiplyByTwo(5); -> 10
-// ex: multiplyByTwo(6); -> 12
+console.log("MultiplyByTwo:", multiplyByTwo(5)); // 10
+console.log("MultiplyByTwo:",multiplyByTwo(6)); // 12
 function multiplyByTwo(numero) {
-  //CODE HERE
+  return numero * 2;
 }
 
 // multplies the two inputted numbers together
-// ex: multiply(1,2); -> 2
-// ex: multiply(2,2); -> 4
+console.log("Multiply:", multiply(1,2)); // 2
+console.log("Multiply:", multiply(2,2)); // 4
 function multiply(val1, val2) {
-  //CODE HERE
+  return val1 * val2
 }
 
 // multiplies all three inputted numbers together
-// ex: multiplyy(1,2,3); -> 6
-// ex: multiplyy(2,2,3); -> 12
+console.log("Multiplyy:", multiplyy(1,2,3)); // 6
+console.log("Multiplyy:", multiplyy(2,2,3)); // 12
 function multiplyy(a, b, c) {
-  //CODE HERE
+  return a * b * c
 }
 
 // returns the square of the inputted number
-// ex: square(5); -> 25
+console.log("Square:", square(5)); // 25
 function square(value) {
-  //CODE HERE
+  return value * value;
 }
 
 // determines if the number is odd
 // outputs true if the number is odd
 // outputs false if the number is even
-// ex: isOdd(4); -> false
-// ex: isOdd(5); -> true
+console.log("-----isOdd Test Cases-----")
+console.log(isOdd(4)); // false
+console.log(isOdd(5)); // true
 function isOdd(num) {
-  //CODE HERE
+  return !!(num % 2)
 }
 
 // determines if the number is negative
 // outputs true if the number is negative
 // outputs false if the number is zero or positive
-// ex: isNegative(-1); -> true
-// ex: isNegative(1); -> false
-// ex: isNegative(0); -> false
+console.log("-----isNegative Test Cases-----")
+console.log(isNegative(-1)); // true
+console.log(isNegative(1)); // false
+console.log(isNegative(0)); // false
 function isNegative(num) {
-  //CODE HERE
+  return !!(num < 0);
 }
-
 // returns the absolute value of the inputted number
-// ex: positive(-1); -> 1
-// ex: positive(1); -> 1
+console.log("-----Positive Test Cases-----")
+console.log(positive(-1)); // 1
+console.log(positive(1)); // 1
 function positive(num) {
-  //CODE HERE
+  return Math.abs(num);
 }
 
 // Returns boolean of whether argument is classified as a Number object
-// isNumber(5); → true
-// isNumber('hi'); → false
+console.log("-----isNumber Test Cases-----")
+console.log(isNumber(5)); // true
+console.log(isNumber('hi')); // false
 function isNumber(value) {
-  //CODE HERE
+  return !!(typeof(value) === 'number')
 }
 
 // Returns boolean of whether argument is classified as an Array object
-// isArray(5); → false
-// isArray([1,2,3]); → true
+console.log("-----isArray Test Cases-----")
+console.log(isArray(5)); // false
+console.log(isArray([1,2,3])); // true
 function isArray(value) {
-  //CODE HERE
+  return !!(Array.isArray(value))
 }
 
 // Returns boolean of whether argument is classified as an Object
-// isObject(5); → false
-// isObject([1,2,3]); → true
+console.log("-----isObject Test Cases-----")
+console.log(isObject(5)); // false
+console.log(isObject([1,2,3])); // true
 function isObject(value) {
-  //CODE HERE
+  return !!(typeof(value) === 'object')
 }
 
 // return boolean of whether argument is classified as null
-// isNull(null); -> true
-// isNull(5); -> false
+console.log("-----isNull Test Cases-----")
+console.log(isNull(null)); // true
+console.log(isNull(5)); // false
 function isNull(value) {
-  //CODE HERE
+  return !!(value === null)
 }
+
